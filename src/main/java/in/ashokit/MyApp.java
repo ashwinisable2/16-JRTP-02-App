7729ee5595f2f5603a81ece01db704a37fb792d0
Ashwini <ashwinikanawade91@gmail.com>
2022-09-19 13:04:31 +0530

    Added SOP Statement

--- src/main/java/in/ashokit/MyApp.java
+++ src/main/java/in/ashokit/MyApp.java
@@ -1,5 +1,9 @@
 package in.ashokit;
 
 public class MyApp {
+	public static void main(String[] args) {
+		System.out.println("Welcome to my Book Store....");
+		System.out.println("We hava many offer...!!");
+	}
 
 }
